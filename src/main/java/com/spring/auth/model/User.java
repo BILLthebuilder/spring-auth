5cae1acb29de0f2548cf72614ce7856c9134ca50
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2022-08-03 16:29:04 +0300

    create the user entity model

---
+++ src/main/java/com/spring/auth/model/User.java
@@ -0,0 +1,72 @@
+package com.spring.auth.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+import org.hibernate.annotations.SQLDelete;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@SQLDelete(sql = "UPDATE users SET status=false WHERE id=?")
+@Table(name = "users")
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false, columnDefinition = "VARCHAR(15)")
+    String firstName;
+
+    @Column(name = "last_name", nullable = false, columnDefinition = "VARCHAR(15)")
+    String lastName;
+
+    @Column(name = "email", unique = true, nullable = false, columnDefinition = "VARCHAR(50)")
+    String email;
+
+    @Column(name = "password", nullable = false, columnDefinition = "VARCHAR(255)")
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @JsonIgnore
+    String password;
+
+    @Column(name = "phone_number", unique = true, nullable = false, columnDefinition = "VARCHAR(50)")
+    String phoneNumber;
+
+    @Column(name = "date_created", updatable = false, columnDefinition = "DATETIME DEFAULT CURRENT_TIMESTAMP")
+    @Temporal(value = TemporalType.TIMESTAMP)
+    private Date dateCreated;
+
+    @PrePersist
+    void dateCreatedAt() {
+        this.dateCreated = new Date();
+    }
+
+    @Column(name = "date_updated", columnDefinition = "DATETIME ON UPDATE CURRENT_TIMESTAMP")
+    @Temporal(value = TemporalType.TIMESTAMP)
+    private Date dateUpdated;
+
+    @PreUpdate
+    void dateUpdatedAt() {
+        this.dateUpdated = new Date();
+    }
+
+    @Column(name = "status", nullable = false, columnDefinition = "TINYINT(1) DEFAULT 1")
+    Boolean status;
+
+    /**
+     * Ensures status value is also updated in the
+     * current session during deletion
+     */
+    @PreRemove
+    public void deleteUser () {
+        this.status = false;
+    }
+
+}
