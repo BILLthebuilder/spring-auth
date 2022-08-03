b0b002a733acb67945822f56c2ab30dfd08a729f
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2022-08-03 16:25:47 +0300

    enable datasource check

--- src/main/java/com/spring/auth/AuthApplication.java
+++ src/main/java/com/spring/auth/AuthApplication.java
@@ -3,8 +3,11 @@ package com.spring.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Profile;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
 public class AuthApplication {
 
 	public static void main(String[] args) {
