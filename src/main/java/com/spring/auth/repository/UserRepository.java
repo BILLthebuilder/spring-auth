4f7b7438e3c51d84a765dcd6f1e4c5b07b97348c
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2022-08-12 08:06:54 +0300

    create basic user repository

---
+++ src/main/java/com/spring/auth/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.spring.auth.repository;
+
+import com.spring.auth.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+    User findById(long id);
+    User findByEmail(String email);
+}
