ad3dc74c44e9d9632c9823f83d62daa8a82c55f6
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2022-09-30 16:01:03 +0300

    make user object optional

--- src/main/java/com/spring/auth/repository/UserRepository.java
+++ src/main/java/com/spring/auth/repository/UserRepository.java
@@ -4,9 +4,11 @@ import com.spring.auth.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
-    User findById(long id);
-    User findByEmail(String email);
-    User deleteUserBy(long id);
+    Optional<User> findById(long id);
+    Optional<User> findByEmail(String email);
+    Optional<User> deleteUserBy(long id);
 }
