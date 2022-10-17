49e78b372f59dadeb6494eb4e7d8b6d47ec86aa3
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2022-10-17 15:36:20 +0300

    fix method error

--- src/main/java/com/unknown/repository/UserRepository.java
+++ src/main/java/com/unknown/repository/UserRepository.java
@@ -1,14 +1,17 @@
 package com.unknown.repository;
 
 import com.unknown.model.User;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findById(long id);
     Optional<User> findByEmail(String email);
-    Optional<User> deleteUserBy(long id);
+    Optional<User> deleteUserById(long id);
 }
+
