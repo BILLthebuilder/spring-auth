4ece0767843ad8dfb770355f1681ff4654a62f2d
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2022-10-17 15:35:52 +0300

    update SecurityConfig

--- src/main/java/com/unknown/configuration/security/SecurityConfig.java
+++ src/main/java/com/unknown/configuration/security/SecurityConfig.java
@@ -1,22 +1,41 @@
 package com.unknown.configuration.security;
 
+import com.nimbusds.jose.jwk.source.ImmutableSecret;
 import com.unknown.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationEntryPoint;
+import org.springframework.security.oauth2.server.resource.web.access.BearerTokenAccessDeniedHandler;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Repository;
 
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 import javax.servlet.http.HttpServletResponse;
+
+import java.nio.charset.StandardCharsets;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 
 import static java.lang.String.format;
 
@@ -28,6 +47,15 @@ public class SecurityConfig {
 
     private final UserRepository userRepository;
 
+//    @Value("${jwt.public.key}")
+//    private RSAPublicKey rsaPublicKey;
+//
+//    @Value("${jwt.private.key}")
+//    private RSAPrivateKey rsaPrivateKey;
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
     public SecurityConfig(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
@@ -35,23 +63,17 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         //Enable CORS and disable CSRF
-        http = http.cors().and().csrf().disable();
-        http = http
+        http.cors().and().csrf().disable();
+        http
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and();
         // Set unauthorized requests exception handler
-        http = http
-                .exceptionHandling()
-                .authenticationEntryPoint(
-                        (request, response, ex) -> {
-                            response.sendError(
-                                    HttpServletResponse.SC_UNAUTHORIZED,
-                                    ex.getMessage()
-                            );
-                        }
-                )
-                .and();
+        http.exceptionHandling(
+                (exceptions) ->
+                        exceptions
+                                .authenticationEntryPoint(new BearerTokenAuthenticationEntryPoint())
+                                .accessDeniedHandler(new BearerTokenAccessDeniedHandler()));
         http
                 .authorizeHttpRequests((authz) -> {
                     try {
@@ -62,36 +84,60 @@ public class SecurityConfig {
                                 .antMatchers("/api/auth/**")
                                 .permitAll()
                                 .anyRequest()
-                                .authenticated();
+                                .authenticated()
+                                .and()
+                                .httpBasic(Customizer.withDefaults())
+                                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
                     } catch (Exception e) {
                         log.error("Filter Chain error={}", e);
                     }
                 });
-        // Add JWT token filter
-        http.addFilterBefore(
-                tokenAuthenticationFilter(),
-                UsernamePasswordAuthenticationFilter.class
-        );
         return http.build();
     }
 
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(username -> (UserDetails) userRepository
-                .findByEmail(username)
-                .orElseThrow(
-                        () -> new UsernameNotFoundException(
-                                format("User: %s, not found", username)
-                        )
-                ));
+    @Bean
+    public AuthenticationManager authenticationManager(
+            HttpSecurity http, BCryptPasswordEncoder bCryptPasswordEncoder) throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(
+                        username ->
+                                (UserDetails) userRepository
+                                        .findByEmail(username)
+                                        .orElseThrow(
+                                                () ->
+                                                        new UsernameNotFoundException(format("User: %s, not found", username))))
+                .passwordEncoder(bCryptPasswordEncoder)
+                .and()
+                .build();
+    }
+
+    @Bean
+    public JwtEncoder jwtEncoder() {
+//        var jwk = new RSAKey.Builder(this.rsaPublicKey).privateKey(this.rsaPrivateKey).build();
+//        var jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+//        return new NimbusJwtEncoder(jwks);
+        var key = new SecretKeySpec(jwtSecret.getBytes(StandardCharsets.UTF_8), "HmacSHA256");
+        var jwks = new ImmutableSecret<>(key);
+        return new NimbusJwtEncoder(jwks);
+
+    }
+
+    // Used by JwtAuthenticationProvider to decode and validate JWT tokens
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        var key = new SecretKeySpec(jwtSecret.getBytes(StandardCharsets.UTF_8), "HmacSHA256");
+        //return NimbusJwtDecoder.withPublicKey(this.rsaPublicKey).build();
+        return NimbusJwtDecoder.withSecretKey(key).build();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-    @Bean
-    public TokenAuthenticationFilter tokenAuthenticationFilter() {
-        return new TokenAuthenticationFilter();
-    }
 
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 }
