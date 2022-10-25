c3bdf40c647d85270b1c10ef88715d4072a5e666
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2022-10-25 22:25:40 +0300

    modify create user dto

--- src/main/java/com/unknown/dto/CreateUserRequest.java
+++ src/main/java/com/unknown/dto/CreateUserRequest.java
@@ -5,30 +5,19 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 
 public record CreateUserRequest(
-        @NotEmpty(message = "First Name cannot be blank")
+        @NotEmpty(message = "FirstName is blank")
         String firstName,
-        @NotEmpty(message = "Last Name cannot be blank")
+        @NotEmpty(message = "LastName is blank")
         String lastName,
         @NotEmpty(message = "Email cannot be blank")
         @Email(message = "Invalid Email Address")
         String email,
         @NotEmpty
-        @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,32}$",
-                message = "Password must have least 8 characters,at least one uppercase letter, one lowercase letter and one number")
+        @Pattern(regexp = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,32}$",
+                message = "Password must have least 8 characters," +
+                        "at least one uppercase letter, one lowercase letter,one special character(@,*) and one number")
         String password,
         @NotEmpty
-        @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,32}$",
-                message = "Password must have least 8 characters,at least one uppercase letter, one lowercase letter and one number")
-        String repeatPassword,
-        @NotEmpty
-        //@Pattern(regexp = "")
         String phoneNumber) {
-
-//    @SneakyThrows
-//    public CreateUserRequest {
-//      if(!password.equalsIgnoreCase(repeatPassword)){
-//         throw new ValidationException("Passwords do not match");
-//      }
-//    }
 
 }
