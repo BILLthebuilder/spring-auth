557d1ab1c8a03409aef00ba55979a9105314a39c
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2022-10-25 22:25:07 +0300

    comment out seed data

--- src/main/java/com/unknown/SeedData.java
+++ src/main/java/com/unknown/SeedData.java
@@ -1,33 +1,33 @@
-package com.unknown;
-
-import com.unknown.model.User;
-import com.unknown.repository.UserRepository;
-import lombok.AllArgsConstructor;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Component;
-import lombok.extern.slf4j.Slf4j;
-
-import javax.transaction.Transactional;
-
-@Transactional
-@Component
-@AllArgsConstructor
-@Slf4j
-public class SeedData implements CommandLineRunner {
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
-
-
-    @Override
-    public void run(String... args) throws Exception {
-       var user = new User();
-       user.setFirstName("Bill");
-       user.setLastName("Kariri");
-       user.setEmail("bill@email.com");
-       user.setPassword(passwordEncoder.encode("123456"));
-       user.setPhoneNumber("254701123456");
-       log.info("Creating user={}",String.valueOf(user));
-       userRepository.save(user);
-    }
-}
+//package com.unknown;
+//
+//import com.unknown.model.User;
+//import com.unknown.repository.UserRepository;
+//import lombok.AllArgsConstructor;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.stereotype.Component;
+//import lombok.extern.slf4j.Slf4j;
+//
+//import javax.transaction.Transactional;
+//
+//@Transactional
+//@Component
+//@AllArgsConstructor
+//@Slf4j
+//public class SeedData implements CommandLineRunner {
+//    private final UserRepository userRepository;
+//    private final PasswordEncoder passwordEncoder;
+//
+//
+//    @Override
+//    public void run(String... args) throws Exception {
+//       var user = new User();
+//       user.setFirstName("Bill");
+//       user.setLastName("Kariri");
+//       user.setEmail("bill@email.com");
+//       user.setPassword(passwordEncoder.encode("123456"));
+//       user.setPhoneNumber("254701123456");
+//       log.info("Creating user={}",String.valueOf(user));
+//       userRepository.save(user);
+//    }
+//}
