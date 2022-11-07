26057c01ccb9694512c4cbe62077ff9b33dd4255
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2022-11-07 15:10:51 +0300

    update error message semantics

--- src/main/java/com/unknown/dto/CreateUserRequest.java
+++ src/main/java/com/unknown/dto/CreateUserRequest.java
@@ -5,19 +5,19 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 
 public record CreateUserRequest(
-        @NotEmpty(message = "FirstName is blank")
+        @NotEmpty(message = "firstName cannot be blank")
         String firstName,
-        @NotEmpty(message = "LastName is blank")
+        @NotEmpty(message = "lastName cannot be blank")
         String lastName,
-        @NotEmpty(message = "Email cannot be blank")
+        @NotEmpty(message = "email cannot be blank")
         @Email(message = "Invalid Email Address")
         String email,
-        @NotEmpty
+        @NotEmpty(message = "password cannot be blank")
         @Pattern(regexp = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,32}$",
                 message = "Password must have least 8 characters," +
                         "at least one uppercase letter, one lowercase letter,one special character(@,*) and one number")
         String password,
-        @NotEmpty
+        @NotEmpty(message = "phoneNumber cannot be blank")
         String phoneNumber) {
 
 }
