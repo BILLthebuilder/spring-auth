9897fe4f3c200955b4d27f9ce4458a502451c58d
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2023-01-11 08:52:17 +0300

    add new columns and change pkey to uuid

--- src/main/java/com/unknown/model/User.java
+++ src/main/java/com/unknown/model/User.java
@@ -3,41 +3,55 @@ package com.unknown.model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
 import org.hibernate.annotations.SQLDelete;
+
 
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
+import java.util.UUID;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
+@Data
 @Entity
 @SQLDelete(sql = "UPDATE users SET status=false WHERE id=?")
 @Table(name = "users")
 public class User implements Serializable {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false)
-    private Long id;
+    @Column(name = "id", columnDefinition = "BINARY(16)")
+    @GeneratedValue(generator = "custom-uuid")
+    @GenericGenerator(
+            name = "custom-uuid",
+            strategy = "org.hibernate.id.UUIDGenerator",
+            parameters = {
+                    @Parameter(
+                            name = "uuid_gen_strategy_class",
+                            value = "org.hibernate.id.uuid.CustomVersionOneStrategy"
+                    )
+            }
+    )
+    private UUID id;
 
     @Column(name = "first_name", nullable = false, columnDefinition = "VARCHAR(15)")
-    String firstName;
+    private String firstName;
 
     @Column(name = "last_name", nullable = false, columnDefinition = "VARCHAR(15)")
-    String lastName;
+    private String lastName;
 
     @Column(name = "email", unique = true, nullable = false, columnDefinition = "VARCHAR(50)")
-    String email;
+    private String email;
 
     @Column(name = "password", nullable = false, columnDefinition = "VARCHAR(255)")
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @JsonIgnore
-    String password;
+    private String password;
 
     @Column(name = "phone_number", unique = true, nullable = false, columnDefinition = "VARCHAR(50)")
-    String phoneNumber;
+    private String phoneNumber;
+
+    @Column(name = "phone_number", unique = true, nullable = false, columnDefinition = "VARCHAR(255)")
+    private String verficationCode;
 
     @Column(name = "date_created", updatable = false, columnDefinition = "DATETIME DEFAULT CURRENT_TIMESTAMP")
     @Temporal(value = TemporalType.TIMESTAMP)
@@ -58,7 +72,7 @@ public class User implements Serializable {
     }
 
     @Column(name = "status", columnDefinition = "TINYINT(1) DEFAULT 1")
-    Boolean status;
+    private Boolean status;
 
     /**
      * Ensures status value is also updated in the
