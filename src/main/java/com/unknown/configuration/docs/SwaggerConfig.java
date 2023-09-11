b4420939ab38b7c4d2cc95a7a365ca79f729261d
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2023-09-11 09:13:55 +0300

    disable swagger docs temporally

--- src/main/java/com/unknown/configuration/docs/SwaggerConfig.java
+++ src/main/java/com/unknown/configuration/docs/SwaggerConfig.java
@@ -1,25 +1,25 @@
-package com.unknown.configuration.docs;
-
-import io.swagger.v3.oas.models.ExternalDocumentation;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class SwaggerConfig {
-    @Bean
-    public OpenAPI springShopOpenAPI() {
-        return new OpenAPI()
-                .info(new Info().title("AUTHENTICATION API")
-                        .description("An authentication API")
-                        .version("v0.0.1")
-                        .license(new License().name("MIT").url("https://github.com/BILLthebuilder/lending-repayment-api/blob/main/LICENSE")))
-                .externalDocs(new ExternalDocumentation()
-                        .description("SpringShop Wiki Documentation")
-                        .url("https://github.com/BILLthebuilder/lending-repayment-api/blob/main/README.md"));
-    }
-
-}
-
+//package com.unknown.configuration.docs;
+//
+//import io.swagger.v3.oas.models.ExternalDocumentation;
+//import io.swagger.v3.oas.models.OpenAPI;
+//import io.swagger.v3.oas.models.info.Info;
+//import io.swagger.v3.oas.models.info.License;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class SwaggerConfig {
+//    @Bean
+//    public OpenAPI springShopOpenAPI() {
+//        return new OpenAPI()
+//                .info(new Info().title("AUTHENTICATION API")
+//                        .description("An authentication API")
+//                        .version("v0.0.1")
+//                        .license(new License().name("MIT").url("https://github.com/BILLthebuilder/lending-repayment-api/blob/main/LICENSE")))
+//                .externalDocs(new ExternalDocumentation()
+//                        .description("SpringShop Wiki Documentation")
+//                        .url("https://github.com/BILLthebuilder/lending-repayment-api/blob/main/README.md"));
+//    }
+//
+//}
+//
